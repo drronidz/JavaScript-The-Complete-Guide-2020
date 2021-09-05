272c33abdbeafe081b01c6fd9f123d5529aa4fe2
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-05 23:55:20 +0100

    19. The (Un)Importance of Code Order

--- 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
@@ -3,20 +3,20 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+//currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`; // backticks "template lateral"
 let errorMessage = 'An error \n'
 + ' occurred!'
-currentResult = add(15,15)
+
 
 add(1, 2)
 add(5, 5)
 
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber
-}
+currentResult = add(15,15)
 
 outputResult(currentResult, calculationDescription)
 
-
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
