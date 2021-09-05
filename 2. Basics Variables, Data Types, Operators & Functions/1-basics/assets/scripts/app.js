2d55e7cafacdd2fefd00441439bb81101d1200e8
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-05 23:51:54 +0100

    18. Returning Values

--- 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
@@ -8,13 +8,15 @@ currentResult = (currentResult + 10) * 3 / 2 - 1;
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`; // backticks "template lateral"
 let errorMessage = 'An error \n'
 + ' occurred!'
-
-outputResult(currentResult, calculationDescription)
+currentResult = add(15,15)
 
 add(1, 2)
 add(5, 5)
 
 function add(firstNumber, secondNumber) {
-    const result = firstNumber + secondNumber
-    alert('The result is ' + result)
+    return firstNumber + secondNumber
 }
+
+outputResult(currentResult, calculationDescription)
+
+
