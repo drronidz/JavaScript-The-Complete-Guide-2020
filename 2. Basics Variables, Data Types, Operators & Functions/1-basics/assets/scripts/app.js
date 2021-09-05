272ec42869c9c52bd5df1f95e1ad3eca6cdf0bac
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-05 23:47:16 +0100

    16. Adding A Custom Function

--- 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
@@ -10,3 +10,11 @@ let errorMessage = 'An error \n'
 + ' occurred!'
 
 outputResult(currentResult, calculationDescription)
+
+add(1, 2)
+add(5, 5)
+
+function add(firstNumber, secondNumber) {
+    const result = firstNumber + secondNumber
+    alert('The result is ' + result)
+}
