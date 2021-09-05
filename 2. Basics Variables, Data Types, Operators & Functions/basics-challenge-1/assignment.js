47a0dd7d9a67002daed776cca66e02c6ff220f8a
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-05 23:33:26 +0100

    14. Time to Practice Variables, Constants, Operators & Core Data Types (Solution)

---
+++ 2. Basics Variables, Data Types, Operators & Functions/basics-challenge-1/assignment.js
@@ -0,0 +1,15 @@
+// Task One
+let userInput = 20
+let resulet;
+
+// Task Two
+resulet = 18
+
+// Task Three
+resulet = resulet - 10
+resulet = resulet * 2
+resulet = resulet / 4
+
+
+alert(resulet)
+alert(userInput)
