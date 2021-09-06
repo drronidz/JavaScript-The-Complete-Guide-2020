19ac959960b39573228e677e0301da3b9615587f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-06 14:37:58 +0100

    27. Converting Data Types

--- 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
+++ 2. Basics Variables, Data Types, Operators & Functions/1-basics/assets/scripts/app.js
@@ -3,7 +3,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = currentResult + userInput.value
+    currentResult = currentResult.toString() + parseInt(userInput.value)
+    // currentResult = currentResult + +userInput.value
     outputResult(currentResult, '')
     // alert('Hi there!') // Unreachable Code
 }
