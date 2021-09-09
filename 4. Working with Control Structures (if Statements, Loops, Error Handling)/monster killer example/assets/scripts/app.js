839bb6af7676baaefc330b67ffa33a1cf3a31961
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-09 01:44:42 +0100

    17. Time for a Heal Player Functionality!

--- 4. Working with Control Structures (if Statements, Loops, Error Handling)/monster killer example/assets/scripts/app.js
+++ 4. Working with Control Structures (if Statements, Loops, Error Handling)/monster killer example/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const NORMAL_ATTACK_VALUE = 10
 const STRONG_ATTACK_VALUE = 17
 const MONSTER_ATTACK_VALUE = 14
+const HEAL_VALUE = 20
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -8,20 +9,9 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackMonster(mode) {
-    let maxDamage;
-    if(mode === 'NORMAL_ATTACK') {
-        maxDamage = NORMAL_ATTACK_VALUE
-    } else if (mode === 'STRONG_ATTACK') {
-        maxDamage = MONSTER_ATTACK_VALUE
-    }
-
-    const monsterDamage = dealMonsterDamage(maxDamage)
-    currentMonsterHealth -= monsterDamage
-
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
     currentPlayerHealth -= playerDamage
-
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You win!')
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
@@ -31,13 +21,41 @@ function attackMonster(mode) {
     }
 }
 
+function attackMonster(mode) {
+    let maxDamage;
+    if(mode === 'NORMAL_ATTACK') {
+        maxDamage = NORMAL_ATTACK_VALUE
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+
+    const monsterDamage = dealMonsterDamage(maxDamage)
+    currentMonsterHealth -= monsterDamage
+    endRound()
+}
+
 function attackHandler() {
     attackMonster('NORMAL_ATTACK')
+    endRound()
 }
 
 function strongAttackHandler() {
     attackMonster('STRONG_ATTACK')
 }
 
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >=chosenMaxLife- HEAL_VALUE) {
+        alert("You can't heal to more than your max initial health.")
+        healValue = chosenMaxLife - currentPlayerHealth
+    } else {
+        healValue  = HEAL_VALUE
+    }
+    increasePlayerHealth(healValue)
+    currentMonsterHealth += healValue
+    endRound()
+}
+
 attackBtn.addEventListener('click', attackHandler)
 strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
