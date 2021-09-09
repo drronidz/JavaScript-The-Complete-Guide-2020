1012f2ea3aaebccdf64ccf28e2769c7e41d97b5e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-09 23:10:59 +0100

    36. Controlling Loops with break

--- 4. Working with Control Structures (if Statements, Loops, Error Handling)/monster killer example/assets/scripts/app.js
+++ 4. Working with Control Structures (if Statements, Loops, Error Handling)/monster killer example/assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt('Maximum life for you and the monster', '100')
 
 let chosenMaxLife = parseInt(enteredValue)
 let battleLog = []
+let lastLoggedEntry = 0
 
 if ( isNaN(chosenMaxLife) ||chosenMaxLife <= 0) {
     chosenMaxLife = 100;
@@ -230,11 +231,15 @@ function printLogHandler() {
 
     let i = 0
     for (const logEntry of battleLog) {
-        console.log(`# ${i} `)
-        for (const  key in logEntry) {
-            console.log(` ${key} : ${logEntry[key]}`)
+        if (!lastLoggedEntry || lastLoggedEntry === i) {
+            console.log(`# ${i} `)
+            for (const  key in logEntry) {
+                console.log(` ${key} : ${logEntry[key]}`)
+            }
+            lastLoggedEntry = i
         }
-    i++
+        i++
+        break
     }
 }
 
