dab92e668b274ebd8f1d4ea884f887c16d5ee629
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-09 01:00:59 +0100

    16. Adding More if Statements & A Strong Attack Functionality

--- 4. Working with Control Structures (if Statements, Loops, Error Handling)/monster killer example/assets/scripts/app.js
+++ 4. Working with Control Structures (if Statements, Loops, Error Handling)/monster killer example/assets/scripts/app.js
@@ -1,5 +1,6 @@
-const PLAYER_ATTACK_VALUE = 10
-const MONSTER_ATTACK_VALUE = 14;
+const NORMAL_ATTACK_VALUE = 10
+const STRONG_ATTACK_VALUE = 17
+const MONSTER_ATTACK_VALUE = 14
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -7,8 +8,15 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler() {
-    const monsterDamage = dealMonsterDamage(PLAYER_ATTACK_VALUE)
+function attackMonster(mode) {
+    let maxDamage;
+    if(mode === 'NORMAL_ATTACK') {
+        maxDamage = NORMAL_ATTACK_VALUE
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = MONSTER_ATTACK_VALUE
+    }
+
+    const monsterDamage = dealMonsterDamage(maxDamage)
     currentMonsterHealth -= monsterDamage
 
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
@@ -21,7 +29,15 @@ function attackHandler() {
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
         alert('You have a draw!')
     }
+}
 
+function attackHandler() {
+    attackMonster('NORMAL_ATTACK')
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK')
 }
 
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
