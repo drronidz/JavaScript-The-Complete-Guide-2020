5329a54f05cb34bf9d6528f3d848db0a4db601fa
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-10 15:39:56 +0100

    10. Implementing the Core Game Logic

--- 6. More on Functions/basics/app.js
+++ 6. More on Functions/basics/app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK'
 const PAPER = 'PAPER'
 const SCISSORS = 'SCISSORS'
 const DEFAULT_USER_CHOICE = ROCK
+const RESULT_DRAW = 'DRAW'
+const RESULT_PLAYER_WINS = 'PLAYER WINS'
+const RESULT_COMPUTER_WINS = 'COMPUTER WINS'
 
 let gameIsRunning = false
 
@@ -20,12 +23,45 @@ const getPlayerChoice = function() {
     return selection
 }
 
+const getComputerChoice = function() {
+    const randomValue = Math.random()
+    if(randomValue < 0.34) {
+        return ROCK
+    } else if (randomValue < 0.67) {
+        return PAPER
+    } else {
+        return SCISSORS
+    }
+}
+
+const getWinner = function (computerChoice, playerChoice) {
+    if(computerChoice === playerChoice) {
+        return RESULT_DRAW
+    } else if (computerChoice === ROCK && playerChoice === SCISSORS) {
+        return RESULT_COMPUTER_WINS
+    } else if (computerChoice === SCISSORS && playerChoice === ROCK) {
+        return RESULT_PLAYER_WINS
+    } else if (computerChoice === SCISSORS && playerChoice === PAPER) {
+        return RESULT_COMPUTER_WINS
+    } else if (computerChoice === PAPER && playerChoice === SCISSORS) {
+        return RESULT_PLAYER_WINS
+    } else if (computerChoice === PAPER && playerChoice === ROCK) {
+        return RESULT_COMPUTER_WINS
+    } else if (computerChoice === ROCK && playerChoice === PAPER) {
+        return RESULT_PLAYER_WINS
+    }
+}
+
 startGameBtn.addEventListener('click', function () {
     if(gameIsRunning) {
         return
     }
     gameIsRunning = true
     console.log('Game is starting...')
-    const playerSelection = getPlayerChoice()
-    console.log(playerSelection)
+    const playerChoice = getPlayerChoice()
+    const computerChoice = getComputerChoice()
+    const winner = getWinner(computerChoice, playerChoice)
+    console.log(`Player choice is :${playerChoice}`)
+    console.log(`Computer choice is :${computerChoice}`)
+    console.log(`The winner is :${winner}`)
 })
