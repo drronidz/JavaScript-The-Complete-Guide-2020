2ba14d80c8614f81c81e314ae16b0d607b2b25bb
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-10 14:54:48 +0100

    5. Functions are Objects!

--- 6. More on Functions/basics/app.js
+++ 6. More on Functions/basics/app.js
@@ -4,13 +4,16 @@ function startGame() {
     console.log('Game is starting...')
 }
 
-const person = {
-    name: 'Max',
-    greet: function greet() {
-        console.log(`Hello there! ${this.name}`)
-    }
-}
+// const person = {
+//     name: 'Max',
+//     greet: function greet() {
+//         console.log(`Hello there! ${this.name}`)
+//     }
+// }
+//
+// person.greet()
 
-person.greet()
+console.log(typeof startGame)
+console.dir(startGame)
 
 startGameBtn.addEventListener('click', startGame)
