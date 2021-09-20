49b3f5a156c8983fe4babb3108217c266abb8904
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-20 15:11:56 +0100

    4. Factory Functions

--- 15. Advanced Function Concepts/basics/app.js
+++ 15. Advanced Function Concepts/basics/app.js
@@ -31,3 +31,15 @@ function printHobbies(hobbies) {
 printHobbies(hobbies)
 
 //
+function createTaxCalculator(tax) {
+    function calculateTax(amount) {
+        return amount * tax
+    }
+    return calculateTax
+}
+
+const calculateVatAmount = createTaxCalculator(0.19)
+const calculateIncomeTaxAmount = createTaxCalculator(0.25)
+
+console.log(calculateVatAmount(100))
+console.log(calculateIncomeTaxAmount(200))
