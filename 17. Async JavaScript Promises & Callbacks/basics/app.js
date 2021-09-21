495179f2555b90d12f8fd0b8705277e6040c655e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-21 23:21:18 +0100

    6. Getting started with Promises

--- 17. Async JavaScript Promises & Callbacks/basics/app.js
+++ 17. Async JavaScript Promises & Callbacks/basics/app.js
@@ -1,14 +1,30 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!')
+    }, duration)
+  })
+  return promise
+}
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
-      posDATA => {
-    console.log(posDATA)
-  }, error => {
-    console.log(error)
+      positionDATA => {
+        setTimer(2000)
+            .then(data => {
+              console.log(data, positionDATA)
+            })
+      },
+      error => {
+        console.log(error)
+      })
+  setTimer(1000).then(() => {
+    console.log('Timer done!')
   })
-  console.log('Getting position...')
+  console.log('Getting position')
 }
 
 button.addEventListener('click', trackUserHandler);
