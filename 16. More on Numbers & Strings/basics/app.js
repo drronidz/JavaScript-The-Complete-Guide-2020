75f78c0b156cefdb70d89bf145eb9ca265fbd3d7
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-09-21 21:51:12 +0100

    8. Tagged Templates

--- 16. More on Numbers & Strings/basics/app.js
+++ 16. More on Numbers & Strings/basics/app.js
@@ -7,3 +7,24 @@ function randomIntBetween(min , max) {
 }
 
 console.log(randomIntBetween(1,10))
+
+
+// Exploring String methods
+// Tagged Templates
+
+function productDescription(strings, productName, productPrice) {
+    console.log(strings)
+    console.log(productName)
+    console.log(productPrice)
+    let priceCategory = 'cheap'
+    if (productPrice > 20) {
+        priceCategory = 'fair'
+    }
+    return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`
+}
+
+const productName = 'JavaScript Course'
+const productPrice = 29.99
+
+const productOutput = productDescription`This product (${productName}) is ${productPrice}.`
+console.log(productOutput)
